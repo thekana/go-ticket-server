3baaf79eeb82f21da3d6d5192d153a25f5f4d19e
Kanadech Jirapongtanavech <kanadech.king@gmail.com>
2020-11-04 21:57:16 +0700

    added deploy script

---
+++ _AWS_SCRIPTS/deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+if [ -z "$1" ]
+  then
+  echo "Require path to key"
+  exit 1
+fi
+if [ -z "$2" ]
+  then
+  echo "Require user@host"
+  exit 1
+fi
+if ! [ -z "$3" ]
+then
+  echo "BUILDING DOCKER IMAGE"
+  sh ../docker/build.sh
+  echo "BUILD COMPLETE"
+else
+  echo "SKIP BUILDING"
+fi
+
+ssh -i $1 $2 'rm -rf server'
+echo "COPYING IMAGE TO AWS"
+sh copy-image.sh $1 $2
+echo "COPYING DEPENDENCIES TO AWS"
+sh copy-files.sh $1 $2
+echo "DOCKER-COMPOSE"
+ssh -i $1 $2 'cd server/docker; docker-compose down; docker-compose up -d'
