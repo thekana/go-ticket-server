cd84f1df9b1a5f5a4a89a0cb670508c77d906b6d
Kanadech Jirapongtanavech <kanadech.king@gmail.com>
2020-11-04 14:53:00 +0700

    Added SSL successfully

--- load_test/cannon.js
+++ load_test/cannon.js
@@ -1,6 +1,6 @@
 const autocannon = require('autocannon')
 data = {
-    custoken1:  "eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MDQ0MjY5OTIsImlhdCI6MTYwNDQwODk5MiwibmFtZSI6ImN1c3QxIiwicm9sZSI6WyJjdXN0b21lciJdLCJ1aWQiOjR9.toTXgHyns0nxnHHVNw2Et5-0Wgup7ZAMeWAoof_P4PdPcgPCiiQk4q_Eyjk-2xEmXpX_38EMB9SBls1DEqPlSKpueFOWWAVgdoqRMYtwE95YUCd-ab4CsShdAhhBKO_yVFtZM7mYyA_QbPKnYQUYv7-VfyE9ZhKBn0iqAMmSuiUdupbSsDv-u5dEa3YKPYD2Vx92gumcezOug8gWSwQ7-bVvNIQu2VJYnOvf2GHLiHMM3zAU9URDh_mCYo5JabcDupfZYvffEkiR_uaerNcs9EuBwdRFnWmPst_s5izJ7TNCFBLcFVX_PMx1V9y0io8Q2NfbOEYijn_USncfMA4SHA",
+    custoken1:  "eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MDQ0OTM4OTAsImlhdCI6MTYwNDQ3NTg5MCwibmFtZSI6ImN1c3QxIiwicm9sZSI6WyJjdXN0b21lciJdLCJ1aWQiOjR9.Flsq7eBxdjfi6pZMBi2rSkTXSAfAolo-48Lg9tG3o2eDTxNY9sARziUMtz1kUgsqzeLIaorcRtorKxPhhdXeam6TovGo9Q_CfkbrWGdGpdg6QJHpz0XFiHxdhf0bD4vKmH3mpc110iGxlbMjGVDiW7oGMEXSGEneicGRR61HbVLultLYkzHrh2H3QDXP311RgPLCJOkOJpSXOwJ7i3oiGpbe9UetfYSoNMIOQkA-M5apDo4qfKq7fysK5TM1TrVkcWTO-v9w-LYhu2V5Od8dkpWZpe43AkdZXreSllWQpqQI04hmsI596800G6P_aS4qv8puX_xs9Sa1SBgVrZ6_xg",
     eventID1: 1,
     eventID2: 2,
     eventID3: 3,
@@ -8,7 +8,7 @@ data = {
 }
 
 function start() {
-    const url = '54.251.152.89/api/v1/reservation/reserve'
+    const url = 'https://www.ticketeer.ml/api/v1/reservation/reserve'
     autocannon({
         url: url,
         connections: 80, // matters a lot can't be more than a hundred and less than cut off amount
