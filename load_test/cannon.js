de5ba8a8283b5fbe3e1b03a4cda5fb34cd7c05ae
Kanadech Jirapongtanavech <kanadech.king@gmail.com>
2020-11-05 14:05:09 +0700

    Updated load test file to use bearer token instead

--- load_test/cannon.js
+++ load_test/cannon.js
@@ -1,6 +1,6 @@
 const autocannon = require('autocannon')
 data = {
-    custoken1:  "eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MDQ0OTM4OTAsImlhdCI6MTYwNDQ3NTg5MCwibmFtZSI6ImN1c3QxIiwicm9sZSI6WyJjdXN0b21lciJdLCJ1aWQiOjR9.Flsq7eBxdjfi6pZMBi2rSkTXSAfAolo-48Lg9tG3o2eDTxNY9sARziUMtz1kUgsqzeLIaorcRtorKxPhhdXeam6TovGo9Q_CfkbrWGdGpdg6QJHpz0XFiHxdhf0bD4vKmH3mpc110iGxlbMjGVDiW7oGMEXSGEneicGRR61HbVLultLYkzHrh2H3QDXP311RgPLCJOkOJpSXOwJ7i3oiGpbe9UetfYSoNMIOQkA-M5apDo4qfKq7fysK5TM1TrVkcWTO-v9w-LYhu2V5Od8dkpWZpe43AkdZXreSllWQpqQI04hmsI596800G6P_aS4qv8puX_xs9Sa1SBgVrZ6_xg",
+    custoken1:  "eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MDQ1NzA5NTksImlhdCI6MTYwNDU1Mjk1OSwibmFtZSI6ImN1c3QxIiwicm9sZSI6WyJjdXN0b21lciJdLCJ1aWQiOjR9.16JVkqWNG-w4hOkArUP7MYYx9Vydnijr-m3HrxObHmFPn7YbWuNdFyYM4Vn9BTkgwz1It1YLBhnq85m7NVxnMI9l4Qb7_XWzJGySWWjCHHI1VmlSLuG8DWrHl4J5slypawzfW_qGcdsPY3AFn-JhcqK1G3fD824UdiPwbqmv_YniK9zR9TCqFepst5PwKZi--VIUaakPXqAwpKL0fg8v7hws20wm8hvy9J9S8CvVHf_a-_jxkuDOSgYWFOjAYIW76cmO5QzgPBYvDqLtdfWyFrfYGso9R9IIHE-PIBHWmB8f5WkLJ2j96XJSm_fLm0qvO_TmWjRu7r-pu-P9isYKcg",
     eventID1: 1,
     eventID2: 2,
     eventID3: 3,
@@ -13,32 +13,32 @@ function start() {
         url: url,
         connections: 80, // matters a lot can't be more than a hundred and less than cut off amount
         duration: 10,
+        headers: {
+            // by default we add an auth token to all requests
+            'Authorization': "Bearer " + data.custoken1
+        },
         requests: [
             {
                 method: 'POST',
                 body: JSON.stringify({
-                    "authToken": data.custoken1,
                     "eventID": data.eventID1,
                     "amount": 1
                 }),
             }, {
                 method: 'POST',
                 body: JSON.stringify({
-                    "authToken": data.custoken1,
                     "eventID": data.eventID2,
                     "amount": 1
                 }),
             }, {
                 method: 'POST',
                 body: JSON.stringify({
-                    "authToken": data.custoken1,
                     "eventID": data.eventID3,
                     "amount": 1
                 }),
             }, {
                 method: 'POST',
                 body: JSON.stringify({
-                    "authToken": data.custoken1,
                     "eventID": data.eventID4,
                     "amount": 1
                 }),
