0f63c9589fe1feda36b570837baa01e7872309ae
Kanadech Jirapongtanavech <kanadech.king@gmail.com>
2020-11-23 21:32:52 +0700

    run_dev_test.sh to run postgres&redis container if not already running

--- scripts/run_dev_test.sh
+++ scripts/run_dev_test.sh
@@ -7,8 +7,24 @@ killgroup(){
   kill 0
 }
 
-# docker run --rm -p 5432:5432 -e POSTGRES_PASSWORD=postgres --name ticket_reservation_postgres postgres:10-alpine &
-# sleep 3
+if [ ! "$(docker ps -q -f name=ticket_reservation_postgres)" ]; then
+    if [ "$(docker ps -aq -f status=exited -f name=ticket_reservation_postgres)" ]; then
+        # cleanup
+        docker rm ticket_reservation_postgres
+    fi
+    # run your container
+    docker run -d --rm -p 5432:5432 -e POSTGRES_PASSWORD=postgres --name ticket_reservation_postgres postgres:12-alpine
+    sleep 2
+fi
+
+if [ ! "$(docker ps -q -f name=ticket_reservation_redis)" ]; then
+    if [ "$(docker ps -aq -f status=exited -f name=ticket_reservation_redis)" ]; then
+        # cleanup
+        docker rm ticket_reservation_redis
+    fi
+    # run your container
+    docker run -d --rm -p 6379:6379 --name ticket_reservation_redis redis:6-alpine
+fi
 
 docker exec -i ticket_reservation_postgres psql -U postgres -c "drop database if exists ticket_reservation" &&
 docker exec -i ticket_reservation_postgres psql -U postgres -c "create database ticket_reservation"
